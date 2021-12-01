9fff9374877f2deeab5a6297f5ceda47ea4ef503
Tom Sisman <tomsisman@gmail.com>
2021-12-01 20:28:50 +0000

    boom

---
+++ day-1/dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+fs_1.default.readFile('./input.txt', 'utf8', (err, input) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(solutionOne(input));
+    console.log(solutionTwo(input));
+});
+// Yes, I'm taking the piss. 
+const solutionOne = (input) => input.split(/\r?\n/).filter((line, i, array) => i !== 0 && parseInt(line) > parseInt(array[i - 1])).length;
+const solutionTwo = (input) => input.split(/\r?\n/).filter((line, i, array) => i !== 0 && parseInt(array[i + 1]) > parseInt(array[i - 2])).length;
+//# sourceMappingURL=index.js.map
---
+++ day-1/index.ts
@@ -0,0 +1,15 @@
+import fs from "fs";
+
+fs.readFile('./input.txt', 'utf8' , (err, input) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(solutionOne(input))
+  console.log(solutionTwo(input))
+})
+ 
+const solutionOne = (input: string) => input.split(/\r?\n/).filter((line, i, array) => i !== 0 && parseInt(line) > parseInt(array[i - 1])).length;
+
+const solutionTwo = (input: string) => input.split(/\r?\n/).filter((line, i, array) => i !== 0 && parseInt(array[i + 1]) > parseInt(array[i - 2])).length;
+
